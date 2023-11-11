16432509491be8dc20a8b8967816993023c24015
01001110IR <nirstam@gmail.com>
2023-11-11 15:05:21 +0200

    1.0.1

--- front/settings.py
+++ front/settings.py
@@ -185,6 +185,7 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = BASE_DIR / 'static/images'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
